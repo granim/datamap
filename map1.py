2c9d1a977bb200b482383bda72550536728a23e8
Grant Murphy <grantsmurphy@gmail.com>
2019-12-17 12:51:18 -0500

    added choropleth map layer

--- map1.py
+++ map1.py
@@ -2,9 +2,7 @@ import folium
 import pandas
 
 
-map = folium.Map(location=[35.56, -99.90], zoom_start=6, tiles = "Stamen Terrain")
 data = pandas.read_csv("Volcanoes.txt")
-
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
@@ -17,19 +15,18 @@ def color_producer(elevation):
         return 'orange'
     else:
         return 'red'         
-
+map = folium.Map(location=[35.56, -99.90], zoom_start=6, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-# fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-# fg.add_child(folium.Marker(location=[38.01, -99.1], popup="Hi I am a Second Marker", icon=folium.Icon(color='blue')))
-
-
 
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln], popup=str(el) +" meters",
      fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
-   
 
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 1000000 
+else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+# PJG54ERUYT5
 map.add_child(fg)
-
 map.save("Map1.html")
