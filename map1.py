1d5f451bb03571a5abfe98a06652d420af7b62de
Grant Murphy <grantsmurphy@gmail.com>
2019-12-18 10:18:32 -0500

    added layer control panel

--- map1.py
+++ map1.py
@@ -27,6 +27,11 @@ for lt, ln, el in zip(lat, lon, elev):
 fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 1000000 
 else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+
+
 # PJG54ERUYT5
 map.add_child(fg)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
